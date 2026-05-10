1a7619b046296af03f4a647fa2d54d6ee646b444
aryat <aryatsrivastava06@gmail.com>
2026-05-10 18:48:34 +0530

    RESP switch case

--- server/core/parser/resp.go
+++ server/core/parser/resp.go
@@ -2,8 +2,8 @@ package parser
 
 import "errors"
 
-func Decode(data []byte) (interface{}, error) {
-	if data == nil || len(data) == 0 {
+func Decode(data []byte) (any, error) {
+	if len(data) == 0 {
 		return nil, errors.New("no data")
 	}
 	value, _, err := decodeOne(data)
@@ -12,6 +12,42 @@ func Decode(data []byte) (interface{}, error) {
 
 }
 
-func decodeOne(data []byte) (interface{}, int, error) {
+func decodeOne(data []byte) (any, int, error) {
+	if len(data) == 0 {
+		return nil, 0, errors.New("no data")
+	}
+	switch data[0] {
+	case '+':
+		return readSimpleString(data)
+	case '-':
+		return readError(data)
+	case ':':
+		return readInt64(data)
+	case '$':
+		return readBulkString(data)
+	case '*':
+		return readArray(data)
+	}
+
+	return nil, 0, nil
+}
+
+func readArray(data []byte) (any, int, error) {
+	panic("unimplemented")
+}
+
+func readBulkString(data []byte) (any, int, error) {
+	panic("unimplemented")
+}
+
+func readInt64(data []byte) (any, int, error) {
+	panic("unimplemented")
+}
+
+func readError(data []byte) (any, int, error) {
+	panic("unimplemented")
+}
+
+func readSimpleString(data []byte) (any, int, error) {
 	panic("unimplemented")
 }
